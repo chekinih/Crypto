42649c54a801790cf65fa981a4fea6125d8752d9
chekinih <chekini_hakima@yahoo.fr>
2020-03-25 22:37:34 +0100

    Add some comments

--- partie2_projet/client.py
+++ partie2_projet/client.py
@@ -3,74 +3,125 @@
 """
 Created on Sat Mar 14 14:48:46 2020
 
-@author: 11607156
+@author: CHEKINI Hakima && BEN Saada meriam
 """
 
 #!/usr/bin/python           # This is client.py file
 
-import socket               # Import socket module
+import socket               # Importer le module socket 
 
 import os, base64
 import io
-import TP1
+#import DES_cle_64
+#import DES_cle_112
+import DES_cle_168
 
+cleValide='Valide'
+cleInvalide='Fin'
+
+# Fontion qui permet de lire un fichier et de retourner son contenue dans une chaine de caractère
 def read_file(path):
     f=io.open(path,mode='r')
     return str(f.read())
 
-# Fonction qui genere une cle de session de 64 bits
-def generateSecretKey():
-    return base64.b64encode(os.urandom(5))
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 1234               # Reserve a port for your service.
+# Fonction qui genere une cle de session qui peut etre de 64, 112 ou 168 bits selon le parametre n qui est le nombre de bits de la cle
+def generateSecretKey(n):
+    n_octets =  n/8
+    if n_octets == 8:
+        return  base64.b64encode(os.urandom(5))
+    elif n_octets == 14:
+        key = base64.b64encode(os.urandom(10))
+        key14 = ""
+        for i in range (0, n_octets):
+            key14 += key[i]
+        return key14
+    else:
+        key = base64.b64encode(os.urandom(16))
+        key21 = ""
+        for i in range (0, n_octets):
+            key21 += key[i]
+        return key21
+
+# Cette fonction renvoie vrai si le message en parametre vaut "Valide", faux s il vaut "Fin"
+def test_valide(msg):
+    if(msg == cleInvalide):
+        return False
+    if (msg == cleValide):
+        return True
+
+s = socket.socket()                 # Creer un object socket
+host = socket.gethostname()         # Recuperer le nom de la machine locale
+port = 1234                         # Reserver un port
 
 s.connect((host, port))
-print (s.recv(1024))
-msg = 'Hello'  # on rentre des donnees
-s.send(msg) 
-# rcevoir la cle public du serveur
-keyPubServ=s.recv(2000)
-keyPubFileName='clepublicServeur.pub.pem'
-pubKeyFile=open(keyPubFileName,"w")
-pubKeyFile.write(keyPubServ)
-pubKeyFile.close()
-print("J'ai reçu la clé publique du seveur")
-                    # Close the socket when done
-print("Je genere une cle secrete des")
-key_DES=generateSecretKey()
-keyDesFileName='pubDesFile.txt'
-keyDesCryptedFileName='pubDesCryptedFile.txt'
-pubDesFile=open(keyDesFileName, "w")
-# Mettre la cle des generee dans un fichier 
-pubDesFile.write(key_DES)
-pubDesFile.close()
-# crypter la cle des secrete genere par le client avec la cle publique du serveur recue
-os.system('openssl rsautl -encrypt -in '+keyDesFileName+' -inkey  '+keyPubFileName+' -out '+keyDesCryptedFileName+' -pubin ')
-#
-print("j'envoie la cle des cryptee")
-# Envoie de la cle secrete chiffree
-s.send(keyDesCryptedFileName)
-acquittementServeur=s.recv(2000)
-print(acquittementServeur)
-print("Acquittement recu de la part du serveur")
+recu=s.recv(2000)
+test = test_valide(recu)            # Tester si la session est valide
+if(not test):
+    s.send(cleInvalide)
+    print('Fin')
+    s.close
+else:
+    print('Session '+recu)
+    s.send(cleValide)
+    msg = 'Hello'                   # Envoie de message de debut de session
+    s.send(msg) 
+    
+    # rcevoir la cle public du serveur
+    keyPubServ=s.recv(2000)
+    keyPubFileName='clepublicServeur.pub.pem'
+    pubKeyFile=open(keyPubFileName,"w")
+    pubKeyFile.write(keyPubServ)
+    pubKeyFile.close()
+    print("\nJ'ai recu la cle publique du seveur")
+                      
+    # generation de la cle secrete DES, A decommenter si besoin de voir le resultat en utilisant des cles de 64 bits, 112 bits ou 168 bits
+    print("\nJe genere une cle secrete DES")
+    #key_DES=generateSecretKey(64)
+    #key_DES=generateSecretKey(112)
+    key_DES=generateSecretKey(168)
+    
+    # Mettre la cle des generee dans un fichier 
+    keyDesFileName='pubDesFile.txt'
+    keyDesCryptedFileName='pubDesCryptedFile.txt'
+    pubDesFile=open(keyDesFileName, "w")
+    pubDesFile.write(key_DES)
+    pubDesFile.close()
+    
+    # crypter la cle des secrete genere par le client avec la cle publique du serveur recue
+    os.system('openssl rsautl -encrypt -in '+keyDesFileName+' -inkey  '+keyPubFileName+' -out '+keyDesCryptedFileName+' -pubin ')
 
-# Envoie d un message crypte au serveur
-print("J envoie au serveur un message crypte")
-cryptedMessage = TP1.chiffrement_final("HELLO SERVER, HOW ARE YOU!", key_DES)
-s.send(cryptedMessage)
+    print("J'envoie la cle DES cryptee\n")
+    # Envoie de la cle secrete chiffree
+    s.send(keyDesCryptedFileName)
+    
+    # Recevoir l'acquittement du serveur
+    acquittementServeur=s.recv(2000)
+    if(acquittementServeur == "ACK"):
+        print(acquittementServeur +" :Acquittement recu de la part du serveur")
+        
+        # Envoie d un message crypte au serveur
+        print("\nJ'envoie au serveur un message crypte")
+        #cryptedMessage = DES_cle_64.chiffrement_final("HELLO SERVER, HOW ARE YOU!", key_DES)
+        #cryptedMessage = DES_cle_112.chiffrement_DES_112("HELLO SERVER, HOW ARE YOU!", key_DES)
+        cryptedMessage = DES_cle_168.chiffrement_DES_168("How are you server? this message is just to test if our program do the right thing. Take care, see You soon.", key_DES)
+        s.send(cryptedMessage)
+        
+        # Recevoir le message chiffre du serveur
+        cryptedMessage = s.recv(2000)
+        print("\nJ'ai recu le message chiffre, je vais le dechiffrer")
+        #decryptedMessage = DES_cle_64.dechiffrement_final(cryptedMessage,key_DES)
+        #decryptedMessage = DES_cle_112.dechiffrement_DES_112(cryptedMessage,key_DES)
+        decryptedMessage = DES_cle_168.dechiffrement_DES_168(cryptedMessage,key_DES)
+        print("Message dechiffre: "+decryptedMessage)
 
-# Recevoir le message chiffre du serveur
-cryptedMessage = s.recv(2000)
-print("J ai recu le message chiffre, je vais le dechiffrer")
-decryptedMessage = TP1.dechiffrement_final(cryptedMessage,key_DES)
-print("Message dechiffre: "+decryptedMessage)
-
-# Le client invalide la cle
-s.send("j'invalide la cle")
-
-# Reception du message du client disant que la cle est invalide
-cleInvalide = s.recv(2000)
-print(cleInvalide)
-s.close 
+        # Le client invalide la cle
+        s.send(cleInvalide)
+        recu1=s.recv(2000)
+        test = test_valide(recu1)
+        if(not test):
+            print('\nFin')
+            s.close
+        
+   
+   
--- partie2_projet/serveur.py
+++ partie2_projet/serveur.py
@@ -1,75 +1,119 @@
-#!/usr/bin/python           # This is server.py file
+#!/usr/bin/python          
 
-import socket               # Import socket module
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 14 14:48:46 2020
+
+@author: CHEKINI Hakima && BEN Saada meriam
+"""
+
+#!/usr/bin/python                # This is serveur.py file
+
+
+import socket                   # Importerle module socket 
 import os
 import io
-import TP1
+#import DES_cle_64
+#import DES_cle_112
+import DES_cle_168
 
+cleValide='Valide'
+cleInvalide='Fin'
 
+# Fontion qui permet de lire un fichier et de retourner son contenue dans une chaine de caractere
 def read_file(path):
     f=io.open(path,mode='r')
     return str(f.read())
 
+# Cette fonction renvoie vrai si le message en parametre vaut "Valide", faux s il vaut "Fin"
+def test_valide(msg):
+    if(msg == cleInvalide):
+        return False
+    if (msg == cleValide):
+        return True
+    
+#Definition des noms des fichiers des cles
 keyPrvFileName='clePrive.pem'
 keyPubFileName='clepublic.pub.pem'
 keyPrvCryptedFileName='clesPriveeChiffree.pem'
-os.system('openssl genrsa -out '+keyPrvFileName+'  2048')  #generer une cle privee avec openssl
-os.system('openssl rsa -in '+keyPrvFileName+' -pubout -out '+keyPubFileName)   #generer une cle publique avec openssl
-os.system('openssl rsa -in '+keyPrvFileName+' -des3 -out '+keyPrvCryptedFileName) #chiffrer la cle privee
+
+#generer une cle privee avec openssl
+os.system('openssl genrsa -out '+keyPrvFileName+'  2048')  
+
+ #generer une cle publique avec openssl
+os.system('openssl rsa -in '+keyPrvFileName+' -pubout -out '+keyPubFileName)  
+
+#chiffrer la cle privee
+os.system('openssl rsa -in '+keyPrvFileName+' -des3 -out '+keyPrvCryptedFileName) 
 keyPub=read_file('clepublic.pub.pem')
 
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 1234          # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
-
-s.listen(10)                 # Now wait for client connection.
+s = socket.socket()                 # Creer un object socket
+host = socket.gethostname()         # Recuperer le nom de la machine locale
+port = 1234                         # Reserver un port
+s.bind((host, port))                # Faire la liaison avec le port
+s.listen(10)                        # Attendre la connexion du client.
 while True:
-   client, addr = s.accept()     # Establish connection with client.
-   print('Got connection from', addr)
-   client.send('Thank you for connecting')
-   # le serveur recoit le hello du client
-   RequeteDuClient = client.recv(2000) # on recoit 255 caracteres grand max
-   if not RequeteDuClient: # si on ne recoit plus rien
-       break  # on break la boucle (sinon les bips vont se repeter)
-   print (RequeteDuClient,"\a")
-   # envoie de la cle public au client
-   print("j'envoie ma cle publique au client")
-   client.send(keyPub)
-   # Recevoir la cle secrete des du client chiffree
-   keyDesCryptedFileName=client.recv(2000)
-   print("j'ai recu la cle des chiffree du client")
-   keyDesDecryptedFileName='pubDesDecryptedFile.txt'
-   # dechiffrer la cle DES chiffrer avec la cle secret du serveur
-   os.system('openssl rsautl -decrypt -in '+keyDesCryptedFileName+' -inkey  '+keyPrvCryptedFileName+' -out '+keyDesDecryptedFileName)
-   print("je dechiffre la cle secrete DES recu")
-   keyDESDecryptted = read_file(keyDesDecryptedFileName)
-   print("La cle DES du client est: "+ keyDESDecryptted)
-   # Le serveur repond au client avec un message d acquittement
-   print("j'envoie un acquittement au client")
-   client.send('serveur => client : Cle DES recu')
-   
-   # Recevoir le message chiffre du client
-   cryptedMessage = client.recv(2000)
-   print("J ai recu le message chiffre, je vais le dechiffrer")
-   decryptedMessage = TP1.dechiffrement_final(cryptedMessage,keyDESDecryptted)
-   print("Message dechiffre: "+decryptedMessage)
-   
-   
-   # Le serveur tente d envoyer lui aussi un message
-   print("J envoie au client un message crypte")
-   cryptedMessage = TP1.chiffrement_final("Hi there client, i m fine thanks", keyDESDecryptted)
-   client.send(cryptedMessage)
-   
-   # Reception du message du client disant que la cle est invalide
-   cleInvalide = client.recv(2000)
-   print(cleInvalide)
-   
-   # Le serveur invalide la cle
-   client.send("j'invalide la cle")
-   
-   # Si le client n envoie plus de requetes, le serveur arrete d ecouter
-   if not client.recv(2000):
+   client, addr = s.accept()        # Etablir la connexion avec le client
+   print('Connexion avec ', addr)
+   client.send(cleValide)
+   recu=client.recv(2000)
+   test = test_valide(recu)        # Tester si le client invalide la cle
+   if(not test):
+       client.close
+       print('Fin')
        break
-   client.close()                # Close the connection
+   else:
+       
+       print ('\nSession '+recu)
+       # Le serveur recoit le hello du client
+       RequeteDuClient = client.recv(2000) 
+       print("J'ai recu "+RequeteDuClient+ " de la part du client\n")
+       
+       # Envoie de la cle public au client
+       print("J'envoie ma cle publique au client")
+       client.send(keyPub)
+       
+       # Recevoir la cle secrete DES du client chiffree
+       keyDesCryptedFileName=client.recv(2000)
+       print("j'ai recu la cle DES chiffree du client\n")
+       
+       # dechiffrer la cle DES chiffrer avec la cle secret du serveur
+       keyDesDecryptedFileName='pubDesDecryptedFile.txt'
+       os.system('openssl rsautl -decrypt -in '+keyDesCryptedFileName+' -inkey  '+keyPrvCryptedFileName+' -out '+keyDesDecryptedFileName)
+       print("\nJe dechiffre la cle secrete DES recue")
+       keyDESDecryptted = read_file(keyDesDecryptedFileName)
+       print("La cle DES du client est: "+ keyDESDecryptted)
+       
+       # Le serveur repond au client avec un message d acquittement
+       print("\nJ'envoie un acquittement au client")
+       client.send('ACK')
+       
+       # Recevoir le message crypte du client
+       cryptedMessage = client.recv(2000)
+       print("\nJ'ai recu le message chiffre, je vais le dechiffrer")
+       #decryptedMessage = DES_cle_64.dechiffrement_final(cryptedMessage,keyDESDecryptted)
+       #decryptedMessage = DES_cle_112.dechiffrement_DES_112(cryptedMessage,keyDESDecryptted)
+       decryptedMessage = DES_cle_168.dechiffrement_DES_168(cryptedMessage,keyDESDecryptted)
+       print("Message dechiffre: "+decryptedMessage)
+       
+       # Le serveur tente d envoyer lui aussi un message
+       print("\nJ'envoie au client un message crypte")
+       #cryptedMessage = DES_cle_64.chiffrement_final("Hi there client, i m fine thanks", keyDESDecryptted)
+       #cryptedMessage = DES_cle_112.chiffrement_DES_112("Hi there client, i m fine thanks", keyDESDecryptted)
+       cryptedMessage = DES_cle_168.chiffrement_DES_168("I am fine client, thanks for asking. If i am answering to you, then your program do the right thing. Bye.", keyDESDecryptted)
+       client.send(cryptedMessage)
+       
+       # Reception du message du client disant que la cle est invalide
+       recu1=client.recv(2000)
+       test = test_valide(recu1)
+       if(not test):
+           client.send(cleInvalide)
+           client.close
+           print('\nFin')
+           break
+       # Si le client n envoie plus de requetes, le serveur arrete d ecouter
+       if not client.recv(2000): 
+           break # on break la boucle (sinon les bips vont se repeter)
+           
